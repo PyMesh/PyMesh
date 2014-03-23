4ecec5239d6f86a5965d353a3a6d690a325f28b6
qz263 <qz263@login-0-1.local>
2014-03-23 14:29:17 -0400

    Minor update to get it compiled on HPC.

--- Settings.cmake
+++ Settings.cmake
@@ -6,7 +6,7 @@ INCLUDE_DIRECTORIES(${PROJECT_SOURCE_DIR}/src)
 SET(CMAKE_BUILD_TYPE Release)
 
 # Create 64 bits binary.  32 bits support is dropped.
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Os")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Os -std=c++11")
 SET(CMAKE_LIBRARY_PATH /opt/local/lib ${CMAKE_LIBRARY_PATH})
 
 # Set output directories
--- tests/learning_tests/opencl/opencl_race_condition.h
+++ tests/learning_tests/opencl/opencl_race_condition.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 #include <numeric>
 #include <string>
 #include <vector>
