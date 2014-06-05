12c72f2cbf46eb5101063402e339351555b6a35c
qnzhou <devnull@localhost>
2014-06-04 23:57:22 -0400

    Minor update on documentation.

--- tools/Assembler/Assemblers/ElasticityTensorAssembler.h
+++ tools/Assembler/Assemblers/ElasticityTensorAssembler.h
@@ -3,6 +3,22 @@
 #include "Assembler.h"
 #include <Assembler/Math/ZSparseMatrix.h>
 
+/**
+ * Assemble matrix that convert flattened strain to flattened stress.
+ * Given the following strain matrix:
+ *      [ e_xx e_xy e_xz ]
+ *      [ e_yx e_yy e_yz ]
+ *      [ e_zx e_zy e_zz ]
+ *
+ * Let C denote the assembled matrix, it satisfy the following equation:
+ *
+ * [sigma_xx]     [e_xx]
+ * [sigma_yy]     [e_yy]
+ * [sigma_zz] = C [e_zz]
+ * [sigma_yz]     [e_yz]
+ * [sigma_zx]     [e_zx]
+ * [sigma_xy]     [e_xy]
+ */
 class ElasticityTensorAssembler : public Assembler {
     public:
         virtual ZSparseMatrix assemble(FESettingPtr setting);
