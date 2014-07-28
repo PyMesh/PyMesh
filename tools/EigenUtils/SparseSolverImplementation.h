1e7a6723d40855bc759c96a09ab78e2fabf374ce
qnzhou <devnull@localhost>
2014-07-28 16:28:19 -0400

    Minor update on error handling.

--- tools/EigenUtils/SparseSolverImplementation.h
+++ tools/EigenUtils/SparseSolverImplementation.h
@@ -3,6 +3,7 @@
 #include <cassert>
 
 #include <Core/EigenTypedef.h>
+#include <Core/Exception.h>
 #include "SparseSolver.h"
 
 template <typename Engine>
@@ -10,17 +11,36 @@ class SparseSolverImplementation : public SparseSolver {
     public:
         virtual void analyze_pattern(const ZSparseMatrix& matrix) {
             m_engine.analyzePattern(matrix);
-            assert(m_engine.info() == Eigen::Success);
+            check_info();
         }
 
         virtual void factorize(const ZSparseMatrix& matrix) {
             m_engine.factorize(matrix);
-            assert(m_engine.info() == Eigen::Success);
+            check_info();
         }
 
         virtual VectorF solve(const VectorF& rhs) {
             auto x = m_engine.solve(rhs);
             return x;
+        }
+
+    private:
+        void check_info() {
+            switch (m_engine.info()) {
+                case Eigen::Success:
+                    return;
+                case Eigen::NumericalIssue:
+                    throw RuntimeError("Eigen sparse solver encounters numerical issue");
+                    break;
+                case Eigen::NoConvergence:
+                    throw RuntimeError("Eigen sparse solver did not converge");
+                    break;
+                case Eigen::InvalidInput:
+                    throw RuntimeError("Eigen sparse solver has invalid input");
+                    break;
+                default:
+                    throw RuntimeError("Unknown eigen sparse solver error");
+            }
         }
 
     protected:
