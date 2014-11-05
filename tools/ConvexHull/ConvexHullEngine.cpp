939f8dceca768ae07cc10956d3ab0b4e05a17d95
qnzhou <devnull@localhost>
2014-11-05 10:12:27 -0500

    Make CGAL dependency optional.

--- tools/ConvexHull/ConvexHullEngine.cpp
+++ tools/ConvexHull/ConvexHullEngine.cpp
@@ -5,9 +5,14 @@
 
 #include <Core/Exception.h>
 
+#ifdef WITH_CGAL
 #include "CGAL/CGALConvexHull2D.h"
 #include "CGAL/CGALConvexHull3D.h"
+#endif
+
+#ifdef WITH_QHULL
 #include "Qhull/QhullEngine.h"
+#endif
 
 namespace ConvexHullEngineHelper {
     bool is_positively_oriented(
@@ -25,9 +30,14 @@ using namespace ConvexHullEngineHelper;
 
 ConvexHullEngine::Ptr ConvexHullEngine::create(
         size_t dim, const std::string& library_name) {
+#ifdef WITH_QHULL
     if (library_name == "qhull") {
         return Ptr(new QhullEngine);
-    } else if (library_name == "cgal") {
+    }
+#endif
+
+#ifdef WITH_CGAL
+    if (library_name == "cgal") {
         if (dim == 2) {
             return Ptr(new CGALConvexHull2D);
         } else if (dim == 3) {
@@ -37,12 +47,13 @@ ConvexHullEngine::Ptr ConvexHullEngine::create(
             err_msg << "CGAL convex hull does not support dim=" << dim;
             throw NotImplementedError(err_msg.str());
         }
-    } else {
-        std::stringstream err_msg;
-        err_msg << "Convex hull library " << library_name
-            << " is not supported.";
-        throw NotImplementedError(err_msg.str());
     }
+#endif
+
+    std::stringstream err_msg;
+    err_msg << "Convex hull library " << library_name
+        << " is not supported.";
+    throw NotImplementedError(err_msg.str());
 }
 
 void ConvexHullEngine::reorient_faces() {
