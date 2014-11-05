d5b8c7869a3bdfe5b68ea2efd798fafab20c4549
qnzhou <devnull@localhost>
2014-11-05 14:47:23 -0500

    Run CGAL convex hull unit tests only if CGAL is detected.

--- tests/tools/ConvexHull/unit_test_driver.cpp
+++ tests/tools/ConvexHull/unit_test_driver.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#ifdef WITH_CGAL
 #include "CGAL/CGALConvexHull2DTest.h"
 #include "CGAL/CGALConvexHull3DTest.h"
+#endif
+#ifdef WITH_QHULL
 #include "Qhull/QhullEngineTest.h"
+#endif
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
