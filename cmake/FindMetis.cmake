0075df924bb68c4f0d4cf8fd508bc5fa5b67eeb6
qz263 <devnull@localhost>
2014-11-11 10:43:15 -0500

    Minor update to get SparseSolver compiled on HPC.

--- cmake/FindMetis.cmake
+++ cmake/FindMetis.cmake
@@ -1,14 +1,14 @@
-# Find Metis library (http://glaros.dtc.umn.edu/gkhome/metis/metis/overview)
+# Find METIS library (http://glaros.dtc.umn.edu/gkhome/METIS/METIS/overview)
 # The following varaibles are set
 #
-# Metis_FOUND
-# Metis_INCLUDE_DIR
-# Metis_LIBRARY
+# METIS_FOUND
+# METIS_INCLUDE_DIRS
+# METIS_LIBRARIES
 #
 # It searches the following environment varaibles
 # $METIS_PATH, $METIS_ROOT, $METIS_INC, $METIS_LIB
 
-FIND_PATH(Metis_INCLUDE_DIR metis.h
+FIND_PATH(METIS_INCLUDE_DIR metis.h
     $ENV{METIS_PATH}
     $ENV{METIS_PATH}/include
     $ENV{METIS_ROOT}
@@ -18,7 +18,7 @@ FIND_PATH(Metis_INCLUDE_DIR metis.h
     /usr/local/include
     /usr/include/)
 
-FIND_LIBRARY(Metis_LIBRARY metis
+FIND_LIBRARY(METIS_LIBRARY metis
     $ENV{METIS_PATH}
     $ENV{METIS_PATH}/lib
     $ENV{METIS_ROOT}
@@ -28,13 +28,15 @@ FIND_LIBRARY(Metis_LIBRARY metis
     /usr/local/lib
     /usr/lib)
 
+SET(METIS_INCLUDE_DIRS ${METIS_INCLUDE_DIR})
+SET(METIS_LIBRARIES ${METIS_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Metis
-    "Metis library cannot be found.  Consider set METIS_PATH environment variable"
-    Metis_INCLUDE_DIR
-    Metis_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(METIS
+    "METIS library cannot be found.  Consider set METIS_PATH environment variable"
+    METIS_INCLUDE_DIRS METIS_LIBRARIES)
 
 MARK_AS_ADVANCED(
-    Metis_INCLUDE_DIR
-    Metis_LIBRARY)
+    METIS_INCLUDE_DIRS
+    METIS_LIBRARIES)
+
