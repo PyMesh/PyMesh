720d637cb9363a7a669fbd936a6ae01da8c2a919
qnzhou <devnull@localhost>
2014-12-31 18:18:23 -0500

    Minor update to avoid redundant bound check.

--- src/Misc/Triplet.h
+++ src/Misc/Triplet.h
@@ -4,32 +4,29 @@
 
 class Triplet {
     public:
-        Triplet(int v1=0, int v2=0, int v3=0) {
-            int v_max = std::max(std::max(v1, v2), v3);
-            int v_min = std::min(std::min(v1, v2), v3);
+        Triplet(int v1=0, int v2=0, int v3=0) : m_ori_data(v1, v2, v3) {
+            int v_max = m_ori_data.maxCoeff();
+            int v_min = m_ori_data.minCoeff();
             int v_mid = v1 + v2 + v3 - v_max - v_min;
-            m_data[0] = v_max;
-            m_data[1] = v_mid;
-            m_data[2] = v_min;
-            m_ori_data[0] = v1;
-            m_ori_data[1] = v2;
-            m_ori_data[2] = v3;
+            m_data.coeffRef(0) = v_max;
+            m_data.coeffRef(1) = v_mid;
+            m_data.coeffRef(2) = v_min;
         }
 
         bool operator==(const Triplet& other) const {
-            return (m_data[0] == other.m_data[0] &&
-                    m_data[1] == other.m_data[1] &&
-                    m_data[2] == other.m_data[2]);
+            return (m_data.coeff(0) == other.m_data.coeff(0) &&
+                    m_data.coeff(1) == other.m_data.coeff(1) &&
+                    m_data.coeff(2) == other.m_data.coeff(2));
         }
 
         bool operator<(const Triplet& other) const {
-            if (m_data[0] < other.m_data[0]) return true;
-            else if (m_data[0] > other.m_data[0]) return false;
+            if (m_data.coeff(0) < other.m_data.coeff(0)) return true;
+            else if (m_data.coeff(0) > other.m_data.coeff(0)) return false;
 
-            if (m_data[1] < other.m_data[1]) return true;
-            else if (m_data[1] > other.m_data[1]) return false;
+            if (m_data.coeff(1) < other.m_data.coeff(1)) return true;
+            else if (m_data.coeff(1) > other.m_data.coeff(1)) return false;
 
-            if (m_data[2] < other.m_data[2]) return true;
+            if (m_data.coeff(2) < other.m_data.coeff(2)) return true;
             else return false;
         }
 
@@ -43,7 +40,7 @@ class Triplet {
             const int p1 = 73856093;
             const int p2 = 19349663;
             const int p3 = 83492791;
-            return (m_data[0]*p1) ^ (m_data[1]*p2) ^ (m_data[2]*p3);
+            return (m_data.coeff(0)*p1) ^ (m_data.coeff(1)*p2) ^ (m_data.coeff(2)*p3);
         }
 
     private:
