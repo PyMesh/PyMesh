dcb768439a4826a61ec9d6b4a7dabb29b528b0da
qnzhou <devnull@localhost>
2015-03-26 16:31:35 -0400

    Added a new test case to PeriodicExploration to gauge how different is the reflection-based inflator from the old inflator.

--- tests/tools/Wires/Interfaces/PeriodicExplorationTest.h
+++ tests/tools/Wires/Interfaces/PeriodicExplorationTest.h
@@ -29,9 +29,27 @@ class PeriodicExplorationTest : public WireTest {
             tree_1.look_up(vertices_2, squared_dist, closest_face_indices);
             return squared_dist.maxCoeff();
         }
+
+        Float area(const Vector3F& v1,
+                   const Vector3F& v2,
+                   const Vector3F& v3) {
+            return ((v2-v1).cross(v3-v1)).norm();
+        }
+
+        Vector3F compute_barycentric_coordinate(
+                const Vector3F& p,
+                const Vector3F& v0,
+                const Vector3F& v1,
+                const Vector3F& v2) {
+            Float a0 = area(p, v1, v2);
+            Float a1 = area(p, v2, v0);
+            Float a2 = area(p, v0, v1);
+            Float total = a0 + a1 + a2;
+            return Vector3F(a0, a1, a2) / total;
+        }
 };
 
-TEST_F(PeriodicExplorationTest, brick5_periodic_timing) {
+TEST_F(PeriodicExplorationTest, DISABLED_brick5_periodic_timing) {
     PeriodicExploration explorer(
             m_data_dir + "brick5.wire", 5, 0.25);
     explorer.with_parameters(
@@ -90,7 +108,7 @@ TEST_F(PeriodicExplorationTest, brick5_periodic_timing) {
     std::cout << " done!" << std::endl;;
 }
 
-TEST_F(PeriodicExplorationTest, brick5) {
+TEST_F(PeriodicExplorationTest, DISABLED_brick5) {
     PeriodicExploration explorer(
             m_data_dir + "brick5.wire", 5, 0.25);
     explorer.with_parameters(
@@ -149,7 +167,7 @@ TEST_F(PeriodicExplorationTest, brick5) {
     std::cout << " done!" << std::endl;;
 }
 
-TEST_F(PeriodicExplorationTest, diamond) {
+TEST_F(PeriodicExplorationTest, DISABLED_diamond) {
     PeriodicExploration explorer(
             m_data_dir + "diamond.wire", 5, 0.5);
     explorer.with_all_parameters();
@@ -200,13 +218,13 @@ TEST_F(PeriodicExplorationTest, diamond) {
     std::cout << " done!" << std::endl;
 }
 
-TEST_F(PeriodicExplorationTest, finite_difference) {
+TEST_F(PeriodicExplorationTest, DISABLED_finite_difference) {
     std::cout << "This might take a few minutes ";
     std::cout.flush();
 
     Float eps = 0.1;
     PeriodicExploration explorer(
-            m_data_dir + "diamond.wire", 5, 0.5);
+            m_data_dir + "pattern0050.wire", 5, 0.5);
     explorer.with_all_parameters();
     explorer.with_refinement("loop", 0);
 
@@ -250,6 +268,7 @@ TEST_F(PeriodicExplorationTest, finite_difference) {
 
         Float dist = compute_hausdorff_distance(
                 perturbed_vertices, init_faces, vertices);
+        std::cout << dist << std::endl;
         ASSERT_LT(dist, eps);
 
         std::cout << ".";
@@ -258,7 +277,7 @@ TEST_F(PeriodicExplorationTest, finite_difference) {
     std::cout << " done!" << std::endl;
 }
 
-TEST_F(PeriodicExplorationTest, gradient_descent) {
+TEST_F(PeriodicExplorationTest, DISABLED_gradient_descent) {
     std::cout << "This might take a few minutes ";
     std::cout.flush();
 
@@ -327,7 +346,7 @@ TEST_F(PeriodicExplorationTest, gradient_descent) {
     std::cout << " done!" << std::endl;
 }
 
-TEST_F(PeriodicExplorationTest, debug) {
+TEST_F(PeriodicExplorationTest, DISABLED_debug) {
     PeriodicExploration explorer(
             m_data_dir + "brick5.wire", 5, 0.5);
     explorer.with_all_parameters();
@@ -367,7 +386,7 @@ TEST_F(PeriodicExplorationTest, DISABLED_debug2) {
     }
 }
 
-TEST_F(PeriodicExplorationTest, printability) {
+TEST_F(PeriodicExplorationTest, DISABLED_printability) {
     PeriodicExploration explorer(
             m_data_dir + "brick5.wire", 5, 0.5);
     explorer.with_all_isotropic_parameters();
@@ -388,7 +407,7 @@ TEST_F(PeriodicExplorationTest, printability) {
     ASSERT_TRUE(explorer.is_printable());
 }
 
-TEST_F(PeriodicExplorationTest, hex_profile) {
+TEST_F(PeriodicExplorationTest, DISABLED_hex_profile) {
     PeriodicExploration explorer(
             m_data_dir + "pattern1065.wire", 5, 0.5);
     explorer.with_all_isotropic_parameters(ParameterCommon::EDGE);
@@ -413,5 +432,116 @@ TEST_F(PeriodicExplorationTest, hex_profile) {
         attr_names.push_back(attr_name);
     }
     save_mesh("hex_1065.msh", mesh, attr_names);
+}
+
+TEST_F(PeriodicExplorationTest, shape_velocity) {
+    const size_t dim = 3;
+    PeriodicExploration explorer(m_data_dir + "pattern1065.wire", 5, 0.5);
+    explorer.with_all_isotropic_parameters(ParameterCommon::EDGE);
+    VectorF dofs(6);
+    dofs << 0.5, 0.5, 0.5, 0.125, -0.125, 0.125;
+    explorer.set_dofs(dofs);
+
+    explorer.periodic_inflate(true);
+    MatrixFr reflected_vertices = explorer.get_vertices();
+    MatrixIr reflected_faces = explorer.get_faces();
+    auto reflected_shape_velocities = explorer.get_shape_velocities();
+
+    explorer.periodic_inflate(false);
+    MatrixFr normal_vertices = explorer.get_vertices();
+    MatrixIr normal_faces = explorer.get_faces();
+    auto normal_shape_velocities = explorer.get_shape_velocities();
+    auto normal_face_sources = explorer.get_face_sources();
+    const size_t num_normal_faces = normal_faces.rows();
+    std::vector<VectorI> faces_with_valid_source;
+    for (size_t i=0; i<num_normal_faces; i++) {
+        if (normal_face_sources[i] != 0) {
+            faces_with_valid_source.push_back(normal_faces.row(i));
+        }
+    }
+    MatrixIr non_periodic_normal_faces = MatrixUtils::rowstack(faces_with_valid_source);
+
+    ASSERT_EQ(reflected_shape_velocities.size(), normal_shape_velocities.size());
+
+    AABBTree tree(normal_vertices, non_periodic_normal_faces);
+
+    VectorF sq_dist;
+    VectorI face_indices;
+    MatrixFr closest_points;
+    tree.look_up_with_closest_points(reflected_vertices,
+            sq_dist, face_indices, closest_points);
+
+    const size_t num_velocities = normal_shape_velocities.size();
+    const size_t num_reflected_vertices = reflected_vertices.rows();
+    ASSERT_EQ(num_reflected_vertices, face_indices.size());
+    ASSERT_EQ(num_reflected_vertices, closest_points.rows());
+
+    std::vector<MatrixFr> interpolated_velocities;
+    std::vector<VectorF> velocity_diff;
+    for (size_t i=0; i<num_velocities; i++) {
+        interpolated_velocities.push_back(
+                MatrixFr::Zero(num_reflected_vertices, dim));
+        velocity_diff.push_back(VectorF::Zero(num_reflected_vertices));
+    }
+    for (size_t i=0; i<num_reflected_vertices; i++) {
+        const size_t fid = face_indices[i];
+        const VectorF& p = closest_points.row(i);
+        const Float dist = sq_dist[i];
+        if (dist > 1e-6) continue;
+
+        const VectorI& f = normal_faces.row(fid);
+        Vector3F barycentric_coord = compute_barycentric_coordinate(
+                p,
+                normal_vertices.row(f[0]),
+                normal_vertices.row(f[1]),
+                normal_vertices.row(f[2]));
+        ASSERT_TRUE(barycentric_coord.allFinite());
+        for (size_t j=0; j<num_velocities; j++) {
+            const VectorF& reflected_velocity =
+                reflected_shape_velocities[j].row(i);
+            VectorF interpolated_velocity =
+                normal_shape_velocities[j].row(f[0]) * barycentric_coord[0] +
+                normal_shape_velocities[j].row(f[1]) * barycentric_coord[1] +
+                normal_shape_velocities[j].row(f[2]) * barycentric_coord[2];
+            interpolated_velocities[j].row(i) = interpolated_velocity;
+            //std::cout << reflected_velocity.transpose() << std::endl;
+            //std::cout << interpolated_velocity.transpose() << std::endl;
+            //ASSERT_NEAR(0.0, (reflected_velocity - interpolated_velocity).norm(), 1e-3);
+            velocity_diff[j][i] = (reflected_velocity - interpolated_velocity).norm();
+        }
+    }
+
+    Mesh::Ptr reflected_mesh = form_mesh(reflected_vertices, reflected_faces);
+    Mesh::Ptr normal_mesh = form_mesh(normal_vertices, normal_faces);
+    for (size_t i=0; i<num_velocities; i++) {
+        std::stringstream attr_name;
+        attr_name << "velocity_" << i;
+        VectorF reflected_velocity =
+                MatrixUtils::flatten<VectorF>(
+                    reflected_shape_velocities[i]);
+        reflected_mesh->add_attribute(attr_name.str());
+        reflected_mesh->set_attribute(attr_name.str(), reflected_velocity);
+
+        VectorF interpolated_velocity =
+                MatrixUtils::flatten<VectorF>(
+                    interpolated_velocities[i]);
+        reflected_mesh->add_attribute(attr_name.str()+"_interpolated");
+        reflected_mesh->set_attribute(attr_name.str()+"_interpolated",
+                interpolated_velocity);
+
+        std::stringstream diff_name;
+        diff_name << "velocity_diff_" << i;
+        reflected_mesh->add_attribute(diff_name.str());
+        reflected_mesh->set_attribute(diff_name.str(), velocity_diff[i]);
+
+        VectorF normal_velocity = MatrixUtils::flatten<VectorF>(
+                normal_shape_velocities[i]);
+        normal_mesh->add_attribute(attr_name.str());
+        normal_mesh->set_attribute(attr_name.str(), normal_velocity);
+    }
+    save_mesh("shape_velocity_compare_reflected.msh", reflected_mesh,
+            reflected_mesh->get_attribute_names());
+    save_mesh("shape_velocity_compare_normal.msh", normal_mesh,
+            normal_mesh->get_attribute_names());
 }
 
