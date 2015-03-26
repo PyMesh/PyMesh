b3f9b630660005c0b3be89b146c1fc3d7eacb679
qnzhou <devnull@localhost>
2015-03-26 14:20:32 -0400

    Put in ifdef guards to present including libigl if it is absent.

--- tools/Boolean/OuterHullEngine.cpp
+++ tools/Boolean/OuterHullEngine.cpp
@@ -1,15 +1,19 @@
 #include "OuterHullEngine.h"
 #include <Core/Exception.h>
+#ifdef WITH_IGL
 #include "IGL/IGLOuterHullEngine.h"
+#endif
 
 #include <sstream>
 
 OuterHullEngine::Ptr OuterHullEngine::create(const std::string& engine_name) {
+#ifdef WITH_IGL
     if (engine_name == "igl") {
         return std::make_shared<IGLOuterHullEngine>();
-    } else {
-        std::stringstream err_msg;
-        err_msg << "Unknown outer hull engine: " << engine_name;
-        throw NotImplementedError(err_msg.str());
     }
+#endif
+
+    std::stringstream err_msg;
+    err_msg << "Unknown outer hull engine: " << engine_name;
+    throw NotImplementedError(err_msg.str());
 }
--- tools/Boolean/SelfIntersectionResolver.cpp
+++ tools/Boolean/SelfIntersectionResolver.cpp
@@ -3,16 +3,20 @@
 #include <sstream>
 #include <Core/Exception.h>
 
+#ifdef WITH_IGL
 #include <IGL/IGLSelfIntersectionResolver.h>
+#endif
 
 SelfIntersectionResolver::Ptr SelfIntersectionResolver::create(
         const std::string& engine_name) {
+#ifdef WITH_IGL
     if (engine_name == "igl") {
         return Ptr(new IGLSelfIntersectionResolver);
-    } else {
-        std::stringstream err_msg;
-        err_msg << "Self-intersection engine \"" << engine_name
-            << "\" is not supported";
-        throw NotImplementedError(err_msg.str());
     }
+#endif
+
+    std::stringstream err_msg;
+    err_msg << "Self-intersection engine \"" << engine_name
+        << "\" is not supported";
+    throw NotImplementedError(err_msg.str());
 }
