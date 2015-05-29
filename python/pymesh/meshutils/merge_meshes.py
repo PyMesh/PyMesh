3d7c8662839e19ba0186b7e47920793e6c90c952
qnzhou <devnull@localhost>
2015-05-29 15:21:59 -0400

    Update merge_meshes script to track source vertices, faces and voxels.

--- python/pymesh/meshutils/merge_meshes.py
+++ python/pymesh/meshutils/merge_meshes.py
@@ -10,36 +10,65 @@ def merge_meshes(input_meshes):
     Returns:
 
         ``Mesh``: An mesh consists of all vertices, faces and voxels
-        from ``input_meshes``.
+        from ``input_meshes``.  The following mesh attributes are defined:
+
+        * ``vertex_sources``: Indices of source vertices from the input mesh.
+        * ``face_sources``: Indices of source faces from the input mesh if the
+          output contains at least 1 face.
+        * ``voxel_sources``: Indices of source voxels from the input mesh if the
+          output contains at least 1 voxel.
     """
     vertices = [];
     faces = [];
     voxels = [];
     vertex_count = 0;
 
-    for mesh in input_meshes:
+    vertex_sources = [];
+    face_sources = [];
+    voxel_sources = [];
+
+    for i,mesh in enumerate(input_meshes):
         vertices.append(mesh.vertices);
+        vertex_sources.append(np.ones(mesh.num_vertices) * i);
         if mesh.num_faces > 0:
             faces.append(mesh.faces + vertex_count);
+            face_sources.append(np.ones(mesh.num_faces) * i);
         if mesh.num_voxels > 0:
             voxels.append(mesh.voxels + vertex_count);
+            voxel_sources.append(np.ones(mesh.num_voxels) * i);
         vertex_count += mesh.num_vertices;
 
     if len(vertices) > 0:
         vertices = np.vstack(vertices);
+        vertex_sources = np.concatenate(vertex_sources);
     else:
         vertices = np.zeros((0, 3), dtype=float);
+        vertex_sources = np.array([]);
 
     if len(faces) > 0:
         faces = np.vstack(faces);
+        face_sources = np.concatenate(face_sources);
     else:
         faces = np.zeros((0, 3), dtype=int)
+        face_sources = np.array([]);
 
     if len(voxels) > 0:
         voxels = np.vstack(voxels);
+        voxel_sources = np.concatenate(voxel_sources);
     else:
         voxels = np.zeros((0, 4), dtype=int);
+        voxel_sources = np.array([]);
 
     output_mesh = form_mesh(vertices, faces, voxels);
+
+    output_mesh.add_attribute("vertex_sources");
+    output_mesh.set_attribute("vertex_sources", vertex_sources);
+    if (len(face_sources) > 0):
+        output_mesh.add_attribute("face_sources");
+        output_mesh.set_attribute("face_sources", face_sources);
+    if (len(voxel_sources) > 0):
+        output_mesh.add_attribute("voxel_sources");
+        output_mesh.set_attribute("voxel_sources", voxel_sources);
+
     return output_mesh;
 
