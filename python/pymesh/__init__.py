608254fb5acac94b239ae7869aaf6e81fad741fc
Qingnan Zhou <qnzhou@gmail.com>
2015-08-24 21:29:32 -0400

    Expose compute_winding_number function at module level.

--- python/pymesh/__init__.py
+++ python/pymesh/__init__.py
@@ -21,6 +21,7 @@ from boolean import boolean
 from selfintersection import resolve_self_intersection
 from selfintersection import detect_self_intersection
 from outerhull import compute_outer_hull
+from winding_number import compute_winding_number
 from meshutils import *
 from misc import *
 from timethis import timethis
