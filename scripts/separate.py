537bc184a31081ed7c93e61077397a851457a87b
Qingnan Zhou <qnzhou@gmail.com>
2015-09-29 16:56:45 -0400

    Minor update to separate script.

--- scripts/separate.py
+++ scripts/separate.py
@@ -2,7 +2,7 @@
 import argparse
 import os.path
 from pymesh import load_mesh, save_mesh
-from pymesh import separate_mesh
+from pymesh import separate_mesh, merge_meshes
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Wrapper around mesh_cc");
@@ -10,6 +10,8 @@ def parse_args():
     parser.add_argument("--connectivity-type", "-c",
             choices=["auto", "vertex", "face", "voxel"],
             help="the meaning of connected", default="auto");
+    parser.add_argument("--highlight", action="store_true",
+            help="Highlight disconnected components");
     parser.add_argument("mesh_in", help="input mesh");
     parser.add_argument("mesh_out", help="output mesh");
     args = parser.parse_args();
@@ -21,10 +23,14 @@ def main():
     mesh = load_mesh(args.mesh_in);
     comps = separate_mesh(mesh, args.connectivity_type);
 
-    basename, ext = os.path.splitext(args.mesh_out);
-    for i,comp in enumerate(comps):
-        filename = "{}_cc{}{}".format(basename, i, ext);
-        save_mesh(filename, comp);
+    if args.highlight:
+        mesh = merge_meshes(comps);
+        save_mesh(args.mesh_out, mesh, *mesh.get_attribute_names());
+    else:
+        basename, ext = os.path.splitext(args.mesh_out);
+        for i,comp in enumerate(comps):
+            filename = "{}_cc{}{}".format(basename, i, ext);
+            save_mesh(filename, comp);
 
 if __name__ == "__main__":
     main();
