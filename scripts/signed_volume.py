1bc806b0352244d298901a2a1bda46c137f55f08
Qingnan Zhou <qnzhou@gmail.com>
2015-10-04 21:01:04 -0400

    Added a script to compute signed volume.

---
+++ scripts/signed_volume.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+This is a simple script to compute the signed volume of an input surface mesh.
+"""
+
+import argparse
+import numpy as np
+import pymesh
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Compute the signed volume");
+    parser.add_argument("input_mesh", help="input mesh");
+    return parser.parse_args();
+
+def main():
+    args = parse_args();
+    mesh = pymesh.load_mesh(args.input_mesh);
+    assert(mesh.dim == 3);
+
+    total_vol = 0.0;
+    origin = np.zeros(3);
+    vertices = mesh.vertices;
+    for f in mesh.faces:
+        corners = vertices[f];
+        volume = np.dot(np.cross(corners[0], corners[1]), corners[2]);
+        total_vol += volume;
+
+    total_vol /= 6.0;
+    print("Total volume: {}".format(total_vol));
+
+if __name__ == "__main__":
+    main();
