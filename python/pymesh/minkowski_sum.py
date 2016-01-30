1696c865a6e9d8dc42db3732de38220356416ca9
Qingnan Zhou <qnzhou@gmail.com>
2016-01-30 11:57:33 -0500

    Add missing doc.

--- python/pymesh/minkowski_sum.py
+++ python/pymesh/minkowski_sum.py
@@ -4,6 +4,15 @@ from .meshio import form_mesh
 from .meshutils import remove_isolated_vertices
 
 def minkowski_sum(mesh, path):
+    """ Perform Minkowski sum of a mesh with a poly-line.
+
+    Args:
+        mesh (:class:`Mesh`): Input mesh.
+        path (:class:`numpy.ndarray`): a :math:`n \times 3` matrix.  Each row
+            represents a node in the poly-line.
+
+    Returns: A mesh representing the Minkowski sum of the inputs.
+    """
     min_sum = PyIGL.MinkowskiSum.create_raw(mesh.vertices, mesh.faces);
     min_sum.run(path);
 
