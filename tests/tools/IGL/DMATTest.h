8a5e85220c81285fafc9b218cac97e6d61d22615
Qingnan Zhou <qnzhou@gmail.com>
2016-04-29 09:48:21 -0400

    Added unit tests for DMAT IO.

---
+++ tests/tools/IGL/DMATTest.h
@@ -0,0 +1,80 @@
+/* This file is part of PyMesh. Copyright (c) 2015 by Qingnan Zhou */
+#pragma once
+
+#include <TestBase.h>
+#include <DMAT.h>
+
+class DMATTest : public TestBase {
+};
+
+TEST_F(DMATTest, MatrixF) {
+    MatrixF A(3, 3);
+    A << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+    std::string tmp_name = "tmp_A.dmat";
+    DMAT::save_matrix(tmp_name, A);
+
+    MatrixF B = DMAT::load_matrix<MatrixF>(tmp_name);
+    ASSERT_MATRIX_EQ(A, B);
+}
+
+TEST_F(DMATTest, MatrixFr) {
+    MatrixFr A(3, 3);
+    A << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+    std::string tmp_name = "tmp_A.dmat";
+    DMAT::save_matrix(tmp_name, A);
+
+    MatrixFr B = DMAT::load_matrix<MatrixFr>(tmp_name);
+    ASSERT_MATRIX_EQ(A, B);
+}
+
+TEST_F(DMATTest, MixedOrder) {
+    MatrixF A(3, 3);
+    A << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+    std::string tmp_name = "tmp_A.dmat";
+    DMAT::save_matrix(tmp_name, A);
+
+    MatrixFr B = DMAT::load_matrix<MatrixFr>(tmp_name);
+    ASSERT_MATRIX_EQ(A, B);
+}
+
+TEST_F(DMATTest, MatrixI) {
+    MatrixI A(3, 3);
+    A << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+    std::string tmp_name = "tmp_A.dmat";
+    DMAT::save_matrix(tmp_name, A);
+
+    MatrixI B = DMAT::load_matrix<MatrixI>(tmp_name);
+    ASSERT_MATRIX_EQ(A, B);
+}
+
+TEST_F(DMATTest, VectorF) {
+    VectorF A(9);
+    A << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+    std::string tmp_name = "tmp_A.dmat";
+    DMAT::save_matrix(tmp_name, A);
+
+    VectorF B = DMAT::load_matrix<VectorF>(tmp_name);
+    ASSERT_MATRIX_EQ(A, B);
+}
+
+TEST_F(DMATTest, ascii) {
+    VectorF A(9);
+    A << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+    std::string tmp_name = "tmp_A.dmat";
+    DMAT::save_matrix(tmp_name, A, true);
+
+    VectorF B = DMAT::load_matrix<VectorF>(tmp_name);
+    ASSERT_MATRIX_EQ(A, B);
+}
---
+++ tests/tools/IGL/unit_test_driver.cpp
@@ -0,0 +1,8 @@
+/* This file is part of PyMesh. Copyright (c) 2016 by Qingnan Zhou */
+#include <gtest/gtest.h>
+#include "DMATTest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
