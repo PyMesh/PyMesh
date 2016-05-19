075daa2ad4338d3eb00c1238dd0a2b7b3cd1ee16
Qingnan Zhou <qnzhou@gmail.com>
2016-05-19 16:02:45 -0400

    Migrate python testing cases out of C++ unit tests.

--- tests/tools/ConvexHull/unit_test_driver.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# This file is part of PyMesh. Copyright (c) 2015 by Qingnan Zhou #
-
-import unittest
-from ConvexHullEngineTest import ConvexHullEngineTest
-
-if __name__ == "__main__":
-    unittest.main();
--- tests/tools/Wires/Tiler/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file is part of PyMesh. Copyright (c) 2015 by Qingnan Zhou #
