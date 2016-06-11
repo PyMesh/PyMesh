462f665e62b5eec2eb9eccbb171a5082589214f6
Qingnan Zhou <qnzhou@gmail.com>
2016-06-11 18:19:25 -0400

    Minor bug fix.

--- cmake/SetInstallRpath.py
+++ cmake/SetInstallRpath.py
@@ -1,17 +1,18 @@
-import site
-site_location = site.getsitepackages();
-site_location.append(site.getusersitepackages());
-site_location.append("${CMAKE_INSTALL_PREFIX}");
-
-import sys
 import os 
+import site
+import sys
 root_dir = os.environ["PYMESH_PATH"];
 package_dir = os.path.join(root_dir, "python/pymesh");
 exec(open(os.path.join(package_dir, 'version.py')).read())
 py_version = sys.version_info;
 install_path = "pymesh-{}-py{}.{}.egg/pymesh/lib".format(
         __version__, py_version[0], py_version[1]);
+
+site_location = site.getsitepackages();
+site_location.append(site.getusersitepackages());
+site_location.append("${CMAKE_INSTALL_PREFIX}");
 site_location = [os.path.join(loc, install_path) for loc in site_location];
+site_location.append(os.path.join(package_dir, "lib"));
 
 with open(os.path.join(root_dir, "cmake/SetInstallRpath.cmake"), 'w') as fout:
     fout.write("SET(CMAKE_INSTALL_RPATH \"{}\")\n".format(";".join(site_location)));
