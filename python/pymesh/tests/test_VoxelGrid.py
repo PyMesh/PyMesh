78e68583e372085d5fb745232840fd67811227b8
Qingnan Zhou <qnzhou@gmail.com>
2016-08-04 08:11:36 -0700

    Update test.

--- python/pymesh/tests/test_VoxelGrid.py
+++ python/pymesh/tests/test_VoxelGrid.py
@@ -7,6 +7,7 @@ class VoxelGridTest(TestCase):
         mesh = generate_icosphere(2.0, [0.0, 0.0, 0.0]);
         grid = VoxelGrid(0.1);
         grid.insert_mesh(mesh);
+        grid.create_grid();
         voxel_mesh = grid.mesh;
 
         self.assertLess(0, voxel_mesh.num_vertices);
