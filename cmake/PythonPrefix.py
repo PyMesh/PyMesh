1ee99f4631f535ca93562426816690a30f3cfa24
Qingnan Zhou <qzhou@adobe.com>
2017-02-11 20:31:18 -0800

    Refactor FindPython.cmake.

--- cmake/PythonPrefix.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)));
-with open(os.path.join(root_dir, "cmake/PythonPrefix.cmake"), 'w') as fout:
-    fout.write("SET(PYTHON_PREFIX \"{}\")\n".format(sys.prefix));
