2a330c847d5f2655864194a73d39328c7d0ab2d8
Qingnan Zhou <qzhou@adobe.com>
2017-03-23 21:45:56 -0700

    Better rpath guessing inspired by https://github.com/qnzhou/PyMesh/issues/30

--- cmake/SetInstallRpath.py
+++ cmake/SetInstallRpath.py
@@ -1,27 +1,45 @@
-import os 
+"""
+RPath generation.
+
+The purpose of this script to guess where pymesh will be installed in the
+system and store the install directories as RPath into the inter-linking
+libraries pymesh generates.
+"""
+
+import os
+import os.path
 import site
 import sys
 root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)));
 package_dir = os.path.join(root_dir, "python/pymesh");
 exec(open(os.path.join(package_dir, 'version.py')).read())
 py_version = sys.version_info;
-install_path = "pymesh-{}-py{}.{}.egg/pymesh/lib".format(
-        __version__, py_version[0], py_version[1]);
-third_party_path = "pymesh-{}-py{}.{}.egg/pymesh/third_party/lib".format(
-        __version__, py_version[0], py_version[1]);
 
-site_location = [];
+install_egg_path = "pymesh-{}-py{}.{}.egg/pymesh".format(
+        __version__, py_version[0], py_version[1]);
+install_path = "pymesh";
+
+site_location = ["${CMAKE_INSTALL_PREFIX}"];
 if hasattr(sys, 'real_prefix'):
     # Within virtualenv.
-    pass
+    site_location.append(os.path.join(sys.prefix,
+        "lib/python{}.{}/site-packages".format(py_version[0], py_version[1])));
+    site_location.append(os.path.join(sys.real_prefix,
+        "lib/python{}.{}/site-packages".format(py_version[0], py_version[1])));
 else:
     # System wide python.
     site_location += site.getsitepackages();
     site_location.append(site.getusersitepackages());
 
-site_location.append("${CMAKE_INSTALL_PREFIX}");
-site_location = [os.path.join(loc, install_path) for loc in site_location] +\
-        [os.path.join(loc, third_party_path) for loc in site_location];
+site_location = \
+        [os.path.join(loc, "{}/lib".format(install_egg_path))
+                for loc in site_location] +\
+        [os.path.join(loc, "{}/third_party/lib".format(install_egg_path))
+                for loc in site_location] +\
+        [os.path.join(loc, "{}/lib".format(install_path))
+                for loc in site_location] +\
+        [os.path.join(loc, "{}/third_party/lib".format(install_path))
+                for loc in site_location];
 site_location.append(os.path.join(package_dir, "lib"));
 site_location.append(os.path.join(package_dir, "third_party/lib"));
 
