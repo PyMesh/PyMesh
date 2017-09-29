833ecc8475dff40c2376362f0e1edb07ac427b44
Qingnan Zhou <qzhou@adobe.com>
2017-09-29 09:49:58 -0700

    Disable some unit tests on module not yet binded.

--- python/PyTriangulation.cpp
+++ python/PyTriangulation.cpp
@@ -11,6 +11,7 @@ using namespace PyMesh;
 
 void init_Triangulation(py::module& m) {
     py::class_<Triangulation, std::shared_ptr<Triangulation> >(m, "Triangulation")
+        .def_static("create", &Triangulation::create)
         .def("set_vertices", &Triangulation::set_vertices)
         .def("run", &Triangulation::run)
         .def("get_faces", &Triangulation::get_faces);
