09d88f604327f13f045c35f6bcfe05e6daa5ffb0
Qingnan Zhou <qzhou@adobe.com>
2017-10-11 17:21:46 -0700

    Update pybind11 and minor bug fix.

--- tools/Wires/Inflator/GeometryCorrectionTable.cpp
+++ tools/Wires/Inflator/GeometryCorrectionTable.cpp
@@ -147,9 +147,9 @@ void GeometryCorrectionTable::apply_correction_to_out_plane_edge(
         const VectorF& proj_v = proj_loop.row(i);
         Float height = proj_edge_dir.dot(proj_v);
         VectorF width_dir = (proj_v - proj_edge_dir * height).normalized();
-        assert(!isnan(width_dir[0]));
-        assert(!isnan(width_dir[1]));
-        assert(!isnan(width_dir[2]));
+        assert(!std::isnan(width_dir[0]));
+        assert(!std::isnan(width_dir[1]));
+        assert(!std::isnan(width_dir[2]));
 
         Float height_sign = (height < 0.0)? -1: 1;
         proj_loop.row(i) = proj_edge_dir * height_sign * half_height
