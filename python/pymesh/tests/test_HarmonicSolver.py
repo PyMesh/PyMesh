511899da1d66356765c2f2faa507489f2436d007
Qingnan Zhou <qzhou@adobe.com>
2018-01-01 19:55:45 -0500

    Update unit test.

--- python/pymesh/tests/test_HarmonicSolver.py
+++ python/pymesh/tests/test_HarmonicSolver.py
@@ -18,7 +18,7 @@ class HarmonicSolverTest(TestCase):
         self.assertLess(0, mesh.num_voxels);
 
         # Test solver to reproduce linear coordinate functions.
-        solver = pymesh.HarmonicSolver.create(mesh.raw_mesh);
+        solver = pymesh.HarmonicSolver.create(mesh);
         bd_indices = np.unique(mesh.faces.ravel());
         bd_values = mesh.vertices[bd_indices, 0];
         solver.boundary_indices = bd_indices;
@@ -44,7 +44,7 @@ class HarmonicSolverTest(TestCase):
         self.assertLess(0, mesh.num_voxels);
 
         # Test solver to reproduce linear coordinate functions.
-        solver = pymesh.HarmonicSolver.create(mesh.raw_mesh);
+        solver = pymesh.HarmonicSolver.create(mesh);
         bd_indices = np.unique(mesh.faces.ravel());
         target_solution = 1.0 / numpy.linalg.norm(mesh.vertices, axis=1);
         bd_values = target_solution[bd_indices];
@@ -82,7 +82,7 @@ class HarmonicSolverTest(TestCase):
         mesh = tri.mesh;
         self.assertLess(0, mesh.num_faces);
 
-        solver = pymesh.HarmonicSolver.create(mesh.raw_mesh);
+        solver = pymesh.HarmonicSolver.create(mesh);
         bd_indices = mesh.boundary_vertices.ravel();
         # f(x,y) = ln(x^2 + y^2) is known to be harmonic.
         target_solution = np.log(np.square(numpy.linalg.norm(mesh.vertices +
