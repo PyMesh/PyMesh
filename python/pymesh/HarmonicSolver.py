915cf4763dee0ba9bf739465d36871f44d20d4c1
Qingnan Zhou <qzhou@adobe.com>
2018-01-01 20:38:55 -0500

    Add documentation.

--- python/pymesh/HarmonicSolver.py
+++ python/pymesh/HarmonicSolver.py
@@ -2,6 +2,37 @@ import PyMesh
 from .meshio import form_mesh
 
 class HarmonicSolver(PyMesh.HarmonicSolver):
+    """ Harmonic PDE solver.
+
+    Input attributes:
+        nodes (:class:``numpy.ndarray``): List of 2D or 3D nodes.
+        elements (:class:``numpy.ndarray``): List of triangles (2D) or tets (3D).
+        order (:class:``numpy.ndarray``): Order of harmonic function.  1 is
+            regular harmonic equation, 2 is biharmonic equation, etc.  Default
+            is 1.
+        boundary_indices (:class:``numpy.ndarray``): Indices into ``nodes``,
+            specifying boundary nodes.
+        boundary_values (:class:``numpy.ndarray``): Function values associated
+            with the boundary node.
+
+    Methods:
+        pre_process(): Run preprocessing step.  (All input attribute must be
+            specified prior of calling this method.)
+        solve(): Run solver.
+
+    Output attributes:
+        solution (:class:``numpy.ndarray``): Solution for the specified harmonic
+            PDE.  (i.e. Solved functions evaluated on the input nodes.)
+
+    Example:
+        >>> solver = pymesh.HarmonicSolver.create(mesh);
+        >>> solver.bd_indices = np.unique(mesh.faces.ravel());
+        >>> solver.bd_values = ...; # Set the corresponding value.
+        >>> solver.pre_process();
+        >>> solver.solve();
+        >>> sol = solver.solution;
+    """
+
     @classmethod
     def create(cls, mesh):
         """ Create a harmonic solver using the input mesh as domain.
--- python/pymesh/meshutils/voxel_utils.py
+++ python/pymesh/meshutils/voxel_utils.py
@@ -2,7 +2,20 @@ import PyMesh
 import numpy as np
 
 def get_tet_orientations_raw(vertices, tets):
+    """ Compute orientation of each tet.
+    Arguments:
+        vertice (``numpy.ndarray``): n by 3 matrix representing vertices.
+        tets (``numpy.ndarray``): m by 4 matrix of vertex indices representing tets.
+
+    Returns:
+        orientations (``numpy.ndarray``): m list of ``float``s, where
+            Positive number => tet is positively oriented.
+                          0 => tet is degenerate.
+            Negative number => tet is inverted.
+    """
     return PyMesh.get_tet_orientations(vertices, tets);
 
 def get_tet_orientations(mesh):
+    """ A thin wrapper of ``get_tet_orientations_raw``.
+    """
     return PyMesh.get_tet_orientations(mesh.vertices, mesh.voxels);
