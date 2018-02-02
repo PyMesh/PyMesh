d98ef4fb8a4ba7126209043270dead1bee8ad9fe
Qingnan Zhou <qzhou@adobe.com>
2018-02-01 22:55:36 -0800

    Typo fix.

--- python/pymesh/tests/test_sparse_solver.py
+++ python/pymesh/tests/test_sparse_solver.py
@@ -7,7 +7,7 @@ import scipy.sparse
 
 class SparseSolverTest(TestCase):
     def test_identity(self):
-        M = scipy.sparse.eye(100).to;
+        M = scipy.sparse.eye(100).tocsc();
         rhs = np.ones(100);
         for solver_type in pymesh.SparseSolver.get_supported_solvers():
             # The following solvers seg faults.  Debug needed.
