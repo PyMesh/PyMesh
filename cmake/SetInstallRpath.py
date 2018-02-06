f751ab52f167dda4a6a1bf635d08ebefcf9c6db8
Qingnan Zhou <qzhou@adobe.com>
2018-02-05 20:54:25 -0800

    Minor update in rpath.

--- cmake/SetInstallRpath.py
+++ cmake/SetInstallRpath.py
@@ -11,10 +11,23 @@ import os.path
 import site
 import sys
 import platform
+import sysconfig
 root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)));
 package_dir = os.path.join(root_dir, "python/pymesh");
 exec(open(os.path.join(package_dir, 'version.py')).read())
 py_version = sys.version_info;
+
+# Python packing is a nightmare...
+install_pathes = ["pymesh", "pymesh2",
+        "pymesh-{}.egg".format(__version__),
+        "pymesh2-{}.egg".format(__version__),
+        "pymesh-{}-py{}.{}.egg".format(__version__, py_version[0], py_version[1]),
+        "pymesh2-{}-py{}.{}.egg".format(__version__, py_version[0], py_version[1]),
+        "pymesh-{}-py{}.{}-{}.egg".format(__version__, py_version[0],
+            py_version[1], sysconfig.get_platform()),
+        "pymesh2-{}-py{}.{}-{}.egg".format(__version__, py_version[0],
+            py_version[1], sysconfig.get_platform()),
+        ];
 
 install_egg_path = "pymesh-{}-py{}.{}.egg/pymesh".format(
         __version__, py_version[0], py_version[1]);
@@ -32,24 +45,23 @@ else:
     site_location += site.getsitepackages();
     site_location.append(site.getusersitepackages());
 
-site_location = \
-        [os.path.join(loc, "{}/lib".format(install_egg_path))
-                for loc in site_location] +\
-        [os.path.join(loc, "{}/third_party/lib".format(install_egg_path))
-                for loc in site_location] +\
-        [os.path.join(loc, "{}/lib".format(install_path))
-                for loc in site_location] +\
-        [os.path.join(loc, "{}/third_party/lib".format(install_path))
-                for loc in site_location];
-site_location.append(os.path.join(package_dir, "lib"));
-site_location.append(os.path.join(package_dir, "third_party/lib"));
+site_locations = [];
+for install_path in install_pathes:
+    for loc in site_location:
+        site_locations.append(os.path.join(loc,
+            "{}/lib".format(install_path)));
+        site_locations.append(os.path.join(loc,
+            "{}/third_party/lib".format(install_path)));
+
+site_locations.append(os.path.join(package_dir, "lib"));
+site_locations.append(os.path.join(package_dir, "third_party/lib"));
 
 if platform.system() == "Linux":
-    site_location.append("$ORIGIN");
-    site_location.append("$ORIGIN/../third_party/lib");
+    site_locations.append("$ORIGIN");
+    site_locations.append("$ORIGIN/../third_party/lib");
 
 with open(os.path.join(root_dir, "cmake/SetInstallRpath.cmake"), 'w') as fout:
-    fout.write("SET(CMAKE_INSTALL_RPATH \"{}\")\n".format(";".join(site_location)));
+    fout.write("SET(CMAKE_INSTALL_RPATH \"{}\")\n".format(";".join(site_locations)));
     fout.write("MESSAGE(STATUS \"CMAKE_INSTALL_RPATH: ${CMAKE_INSTALL_RPATH}\")\n");
 
 
