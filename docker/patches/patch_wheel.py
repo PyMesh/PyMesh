39cbf8f51fe3551097632634087dfbad43cf7aeb
Qingnan Zhou <qzhou@adobe.com>
2018-07-23 23:13:10 -0700

    Import bug fix.

--- docker/patches/patch_wheel.py
+++ docker/patches/patch_wheel.py
@@ -13,8 +13,8 @@ dependencies = [
         "/usr/lib/x86_64-linux-gnu/libgmp.so.10",
         "/usr/lib/x86_64-linux-gnu/libmpfr.so.6",
         "/usr/lib/x86_64-linux-gnu/libgomp.so.1",
-        "/usr/lib/x86_64-linux-gnu/libtbb.so",
-        "/usr/lib/x86_64-linux-gnu/libtbbmalloc.so",
+        "/usr/lib/x86_64-linux-gnu/libtbb.so.2",
+        "/usr/lib/x86_64-linux-gnu/libtbbmalloc.so.2",
         ];
 
 dist_dir = "/root/PyMesh/dist"
