dfb15a26ce997b8dcf05f5f97ce5fffd1f458128
Qingnan Zhou <qzhou@adobe.com>
2018-09-11 11:34:57 -0700

    Update unit tests dependency check.

--- tests/tools/IGL/unit_test_driver.cpp
+++ tests/tools/IGL/unit_test_driver.cpp
@@ -1,7 +1,9 @@
 /* This file is part of PyMesh. Copyright (c) 2016 by Qingnan Zhou */
 #include <gtest/gtest.h>
 #include "DMATTest.h"
+#ifdef WITH_IGL_AND_CGAL
 #include "MinkowskiSumTest.h"
+#endif
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
