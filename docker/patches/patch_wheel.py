6b62c33b52eeb2b2a959fb0d589f546d452df841
Qingnan Zhou <qzhou@adobe.com>
2018-09-25 08:24:15 -0700

    Update patch script and increase docker mem.

--- docker/patches/patch_wheel.py
+++ docker/patches/patch_wheel.py
@@ -5,17 +5,17 @@ import os
 import os.path
 from subprocess import check_call
 
-TBB_2018_US_URL = "https://github.com/01org/tbb/releases/download/2018_U5/tbb2018_20180618oss_lin.tgz";
-cmd = "wget -O tbb.tgz {}".format(TBB_2018_US_URL);
-check_call(cmd.split());
+#TBB_2018_US_URL = "https://github.com/01org/tbb/releases/download/2018_U5/tbb2018_20180618oss_lin.tgz";
+#cmd = "wget -O tbb.tgz {}".format(TBB_2018_US_URL);
+#check_call(cmd.split());
+#
+#cmd = "tar -zxf tbb.tgz";
+#check_call(cmd.split());
 
-cmd = "tar -zxf tbb.tgz";
-check_call(cmd.split());
-
-tbb_libs = [
-        "tbb2018_20180618oss/lib/intel64/gcc4.7/libtbb.so.2",
-        "tbb2018_20180618oss/lib/intel64/gcc4.7/libtbbmalloc.so.2",
-        ];
+#tbb_libs = [
+#        "tbb2018_20180618oss/lib/intel64/gcc4.7/libtbb.so.2",
+#        "tbb2018_20180618oss/lib/intel64/gcc4.7/libtbbmalloc.so.2",
+#        ];
 
 dependencies = [
         "/usr/lib/x86_64-linux-gnu/libboost_system.so.1.62.0",
@@ -27,7 +27,7 @@ dependencies = [
         "/usr/lib/x86_64-linux-gnu/libmpfr.so.6",
         "/usr/lib/x86_64-linux-gnu/libgomp.so.1",
         ];
-dependencies += tbb_libs;
+#dependencies += tbb_libs;
 
 dist_dir = "/root/PyMesh/dist"
 for f in os.listdir(dist_dir):
@@ -38,5 +38,5 @@ for f in os.listdir(dist_dir):
         basename = os.path.basename(dependency);
         zfout.write(dependency, os.path.join("pymesh/lib/", basename));
 
-cmd = "rm -rf tbb2018_20180618oss pstl2018_20180618oss tbb.tgz";
-check_call(cmd.split());
+#cmd = "rm -rf tbb2018_20180618oss pstl2018_20180618oss tbb.tgz";
+#check_call(cmd.split());
