75edbb65e32b900b1566a61f6f84f2f8ee7b9f5d
Qingnan Zhou <qnzhou@gmail.com>
2018-11-14 11:45:02 -0500

    Set favicon.

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,7 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '_static/pymesh_icon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
