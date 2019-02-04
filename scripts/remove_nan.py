38209f1522d331ad3cc25a7adecaeb40b8a1a500
Qingnan Zhou <qnzhou@gmail.com>
2019-02-03 16:53:24 -0800

    Update remove nan script.

--- scripts/remove_nan.py
+++ scripts/remove_nan.py
@@ -28,37 +28,40 @@ def main():
         adj_v = adj_v[np.logical_not(bad_vertex[adj_v])];
         vertices[i] = np.mean(vertices[adj_v,:], axis=0);
 
-    faces = mesh.faces;
-    uv = np.copy(mesh.get_attribute("corner_texture").reshape((-1, 2)));
-    bad_uv = np.logical_not(np.all(np.isfinite(uv), axis=1));
-    bad_uv_indices = np.arange(len(uv), dtype=int)[bad_uv];
-    for i in bad_uv_indices:
-        fi = i // mesh.vertex_per_face;
-        ci = i % mesh.vertex_per_face;
-        vi = faces[fi, ci];
-        adj_f = mesh.get_vertex_adjacent_faces(vi);
-        adj_c = [adj_f[j]*mesh.vertex_per_face +
-                np.argwhere(f==vi).ravel()[0]
-                for j,f in enumerate(faces[adj_f, :])];
-        adj_c = np.array(adj_c, dtype=int);
-        adj_c = adj_c[np.logical_not(bad_uv[adj_c])];
-        if len(adj_c) > 0:
-            uv[i] = np.mean(uv[adj_c,:], axis=0);
-        else:
-            # All corner uv at this vertex is NaN. Average the one-ring instead.
+    out_mesh = pymesh.form_mesh(vertices, mesh.faces);
+
+    if mesh.has_attribute("corner_texture"):
+        faces = mesh.faces;
+        uv = np.copy(mesh.get_attribute("corner_texture").reshape((-1, 2)));
+        bad_uv = np.logical_not(np.all(np.isfinite(uv), axis=1));
+        bad_uv_indices = np.arange(len(uv), dtype=int)[bad_uv];
+        for i in bad_uv_indices:
+            fi = i // mesh.vertex_per_face;
+            ci = i % mesh.vertex_per_face;
+            vi = faces[fi, ci];
+            adj_f = mesh.get_vertex_adjacent_faces(vi);
             adj_c = [adj_f[j]*mesh.vertex_per_face +
-                    (np.argwhere(f==vi).ravel()[0]+1)%mesh.vertex_per_face
-                    for j,f in enumerate(faces[adj_f, :])];
-            adj_c += [adj_f[j]*mesh.vertex_per_face +
-                    (np.argwhere(f==vi).ravel()[0]+2)%mesh.vertex_per_face
+                    np.argwhere(f==vi).ravel()[0]
                     for j,f in enumerate(faces[adj_f, :])];
             adj_c = np.array(adj_c, dtype=int);
             adj_c = adj_c[np.logical_not(bad_uv[adj_c])];
-            uv[i] = np.mean(uv[adj_c,:], axis=0);
+            if len(adj_c) > 0:
+                uv[i] = np.mean(uv[adj_c,:], axis=0);
+            else:
+                # All corner uv at this vertex is NaN. Average the one-ring instead.
+                adj_c = [adj_f[j]*mesh.vertex_per_face +
+                        (np.argwhere(f==vi).ravel()[0]+1)%mesh.vertex_per_face
+                        for j,f in enumerate(faces[adj_f, :])];
+                adj_c += [adj_f[j]*mesh.vertex_per_face +
+                        (np.argwhere(f==vi).ravel()[0]+2)%mesh.vertex_per_face
+                        for j,f in enumerate(faces[adj_f, :])];
+                adj_c = np.array(adj_c, dtype=int);
+                adj_c = adj_c[np.logical_not(bad_uv[adj_c])];
+                uv[i] = np.mean(uv[adj_c,:], axis=0);
 
-    out_mesh = pymesh.form_mesh(vertices, mesh.faces);
-    out_mesh.add_attribute("corner_texture");
-    out_mesh.set_attribute("corner_texture", uv);
+        out_mesh.add_attribute("corner_texture");
+        out_mesh.set_attribute("corner_texture", uv);
+
     pymesh.save_mesh(args.output_mesh, out_mesh);
 
 if __name__ == "__main__":
