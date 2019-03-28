fd14fa09406857416f2c4e37adc7fc9371aa8c33
yhu-radiasoft <yhu@radiasoft.net>
2019-03-28 10:53:10 -0600

    Add signed_distance_to_mesh

--- tools/BVH/IGL/AABBTree.h
+++ tools/BVH/IGL/AABBTree.h
@@ -6,6 +6,7 @@
 #include <BVH/BVHEngine.h>
 
 #include <igl/AABB.h>
+#include <igl/signed_distance.h>
 
 namespace PyMesh {
 namespace IGL {
@@ -29,6 +30,20 @@ class AABBTree : public BVHEngine {
                 MatrixFr& closest_points) const {
             m_tree.squared_distance(m_vertices, m_faces, points,
                     squared_distances, closest_faces, closest_points);
+        }
+
+        virtual void lookup_signed(const MatrixFr& points,
+                const MatrixFr& face_normals,
+                const MatrixFr& vertex_normals,
+                const MatrixFr& edge_normals,
+                const VectorI& edge_map,
+                VectorF& signed_distances,
+                VectorI& closest_faces,
+                MatrixFr& closest_points,
+                MatrixFr& closest_face_normals) const {
+          igl::signed_distance_pseudonormal(points, m_vertices, m_faces, m_tree,
+              face_normals, vertex_normals, edge_normals, edge_map,
+              signed_distances, closest_faces, closest_points, closest_face_normals);
         }
 
     private:
