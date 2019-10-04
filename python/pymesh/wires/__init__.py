3f769b414ab5978a43a4d73a34e73323d0785afe
Qingnan Zhou <qnzhou@gmail.com>
2019-10-03 21:31:04 -0400

    Add merge wires support.

--- python/pymesh/wires/__init__.py
+++ python/pymesh/wires/__init__.py
@@ -3,3 +3,4 @@ from .Tiler import Tiler
 from .Parameters import Parameters
 from .Inflator import Inflator
 from .wires_io import load_wires, form_wires, save_wires
+from .merge_wires import merge_wires
---
+++ python/pymesh/wires/merge_wires.py
@@ -0,0 +1,17 @@
+from . import WireNetwork
+from . import form_wires
+import numpy as np
+
+def merge_wires(wire_networks):
+    """ Merge multiple wire networks into a single WireNetwork object.
+    """
+    vertices = [w.vertices for w in wire_networks];
+    num_vertices = [w.num_vertices for w in wire_networks];
+    offsets = np.cumsum([0] + num_vertices);
+
+    edges = [w.edges + offsets[i] for i,w in enumerate(wire_networks)];
+
+    vertices = np.vstack(vertices);
+    edges = np.vstack(edges);
+    return form_wires(vertices, edges);
+
